9e721c9ed2a924f46a6986c2a83f431dbd7ac812
yeongtaek <144389087+yeonggtaek@users.noreply.github.com>
2025-01-27 10:36:18 -0800

    animation

---
+++ src/components/Animation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export const Animation = () => {
+  return (
+<DotLottieReact
+  src="https://lottie.host/dbfd5756-4a09-418f-ad03-6e18529f261d/4h9WYANPPj.lottie"
+  background="transparent"
+  speed="1"
+  style={{ width: '200px', height: '200px' }}
+  loop
+  autoplay
+/>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,21 @@ import Image from "next/image";
 import { Layout } from "@/components/Layout";
 import { Intro } from "@/components/Intro";
 import { Projects } from "@/components/Projects";
+import { Animation } from "@/components/Animation";
 
 export default function Home() {
   return (
     <Layout>
       <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div className="flex flex-col sm:flex-row items-center gap-[20rem]">
         <div className="text-[30px] font-semibold">
           Hello! I’m Yeongtaek
           <br />
           I’m a Web Designer & Developer!
+        </div>
+        <div>
+          <Animation />
+          </div>
         </div>
         <Intro />
         <div className="text-[46px] font-semibold">
