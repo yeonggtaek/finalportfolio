2275361d4a333842fa24af71f6891128ca40cea2
yeongtaek <144389087+yeonggtaek@users.noreply.github.com>
2025-02-23 00:36:19 -0800

    remedify

--- src/pages/project/remedify/Logo.jsx
+++ src/pages/project/remedify/Logo.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export const Logo = () => {
   return (
-    <div className="flex flex-col gap-[50px] w-[1082px] bg-[#F1F1E7] rounded-[50px] px-[60px] py-[84px]">
+    <div className="flex flex-col gap-[50px] w-[1082px] bg-[#F1F1E7] rounded-[50px] px-[60px] py-[84px] pb-[180px]">
       <div className="text-[#00A39B] text-[64px] font-bold">Logo</div>
-      <Image src="/remedify-logo.png" width={915} height={186} alt="logo" />
+      <Image src="/remedify-logo.png" width={910} height={180} alt="logo" />
     </div>
   );
 };
